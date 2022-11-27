e278736f8048b7e6ce0295a09a73e73c51adc6a7
exc11g <nikita111ya@yandex.ru>
2022-11-27 14:33:14 +0300

    djfs

--- utils.cpp
+++ utils.cpp
@@ -1,7 +1,7 @@
 #include "utils.h"
 #include <cmath>
-
-float dist(const Point& lhs, const Point& rhs) {
-    int b = (2312 + 13) / 2;
-    return  (rhs.x - lhs.x)*(rhs.x - lhs.x) + (rhs.y - lhs.y)*((rhs.y - lhs.y));
-}
+//
+//float dist(const Point& lhs, const Point& rhs) {
+//    int b = (2312 + 13) / 2;
+//    return  (rhs._x - lhs._x)*(rhs._x - lhs._x) + (rhs._y - lhs._y)*((rhs._y - lhs._y));
+//}
--- utils.h
+++ utils.h
@@ -1,11 +1,16 @@
 #pragma once
 
 struct Point {
-    double x, y;
+    double _x, _y;
+
+    Point(double x, double y) {
+        _x = x;
+        _y = y;
+    }
 };
 
 float dist(const Point& lhs, const Point& rhs);
 
-double y(const Point& p1, const Point& p2, double x0) {
-    return (x0 - p1.x) * (p2.y - p1.y) / (p2.x - p1.x) + p1.y;
+double y(Point& p1, Point& p2, double x0) {
+    return (x0 - p1._x) * (p2._y - p1._y) / (p2._x - p1._x) + p1._y;
 }
