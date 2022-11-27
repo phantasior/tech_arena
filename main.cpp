b0ce72edbafb75b2e2166997d3d84db89536fb4f
Artyom Marchenko <phantasiorr@gmail.com>
2022-11-27 12:17:38 +0300

    nic

--- main.cpp
+++ main.cpp
@@ -1,9 +1,33 @@
 #include "utils.h"
 
+#include <fstream>
 #include <iostream>
+#include <vector>
+
+std::vector<Point> points;
 
 int main() {
-    Point a = {0, 0};
-    Point b = {1, 1};
-    std::cout << dist(a, b);
+    freopen("../tests/07.txt", "r", stdin);
+
+    int n;
+    double c1, c2;
+    std::cin >> n >> c1 >> c2;
+    points.resize(n);
+
+    double minx = INT_MAX, miny = INT_MAX, maxx = -INT_MAX, maxy = -INT_MAX;
+
+    for (int i = 0; i < n; ++i) {
+        double x, y;
+        std::cin >> x >> y;
+        points[i] = {x, y};
+
+        minx = std::min(minx, x);
+        miny = std::min(miny, y);
+        maxx = std::max(maxx, x);
+        maxy = std::max(maxy, y);
+    }
+
+    std::cout << 1 << '\n';
+    std::cout << std::fixed << std::setprecision(9);
+    std::cout << minx << ' ' << miny << ' ' << maxx << ' ' << maxy << '\n';
 }
